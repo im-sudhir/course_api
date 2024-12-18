c64f2d266b35214164e4d185f05d6b9acca8793a
Sudhir Vishwakarma <sudhirvi@google.com>
2024-12-18 19:26:01 +0530

    added code to handle post request on Course Model

--- app/views.py
+++ app/views.py
@@ -7,9 +7,17 @@ from .serializers import *
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def course_apiview(request):
     if request.method=="GET":
         courses=Courses.objects.all()
         serializer=CourseSerializer(courses, many=True)
         return Response(serializer.data)
+    
+    if request.method=='POST':
+        serializer=CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
